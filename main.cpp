e65bb3479853e9beee0f8bb53b6687687b67b882
Andrew Lorber <andrewlorber@aol.com>
2019-10-07 18:53:48 -0400

    Adds fnc to initialize new game board

--- main.cpp
+++ main.cpp
@@ -17,6 +17,26 @@ using namespace std;
 // 0 = empty space, 1 = player 1, 2 = player 2, 3 = player 1 king, 4 = player 2 king
 int board[8][8] = {0};
 
+//Initializes new game board
+void initStartBoard(){
+    int player = 2;
+    for(int i = 0, j = 0; j < 8; i++){
+        if ((i+j+1)%2 == 0){
+            board[i][j] = player;
+        }
+        
+        if (i == 7){
+            i = -1;
+            if(j == 2){
+                j = 5;
+                player = 1;
+            } else {
+                j++;
+            }
+        }
+    }
+}
+
 void printBoard(){
     for(int i = 0, j = 0; j < 8; i++){
         cout << " | " << board[i][j];
@@ -29,7 +49,7 @@ void printBoard(){
 }
 
 int main(int argc, const char * argv[]) {
+    initStartBoard();
     printBoard();
     return 0;
 }
-
