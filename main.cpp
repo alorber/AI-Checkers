3d5ddab4d64e4d3216056c0b7726b82759f837b7
Andrew Lorber <andrewlorber@aol.com>
2019-10-10 01:55:39 -0400

    Adds fnc to find legal moves (No jumps yet)

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
 #include <iostream>
 #include <array>
 #include <string>
-#include <stdlib.h>
+#include <vector>
+
 
 using namespace std;
 
@@ -37,6 +38,104 @@ void initStartBoard(){
     }
 }
 
+//Coordinate class to simplify things
+class Coordinate {
+public:
+    int x, y;
+    Coordinate(int i, int j) : x{i}, y{j} {}
+};
+
+void jump(){
+    
+}
+
+// Legal Moves
+vector<vector<Coordinate>> moves;
+vector<vector<Coordinate>*> jumps;
+
+void legalMoves(int player){
+
+    int otherPlayer;
+    bool king;
+    
+    if(player == 1){
+        otherPlayer = 2;
+    } else {
+        otherPlayer = 1;
+    }
+    
+    for(int i = 0, j = 0; j < 8; i++){
+        if(board[i][j] == player || board[i][j] == player +2){
+            
+            king = board[i][j] > 2;
+            
+            // Checks North corners
+            if((player == 1 || king) && j != 0){ // If on top row, then no North corners
+    
+                // check NW
+                if(i != 0){
+                    if(board[i-1][j-1] == 0){
+                        // Adds move
+                        moves.push_back({Coordinate(i, j), Coordinate(i-1, j-1)});
+                    } else if((board[i-1][j-1] == otherPlayer || board[i-1][j-1] == otherPlayer + 2)
+                              && i != 1 && j != 1){
+                        if(board[i-2][j-2] == 0){
+                           // ***Jump
+                        }
+                    }
+                }
+                
+                // check NE
+                if(i != 7){
+                    if(board[i+1][j-1] == 0){
+                        // Adds move
+                        moves.push_back({Coordinate(i, j), Coordinate(i+1, j-1)});
+                    } else if((board[i+1][j-1] == otherPlayer ||board[i+1][j-1] == otherPlayer + 2)
+                              && i != 6 && j != 1){
+                        if(board[i+2][j-2]){
+                           //***Jump
+                        }
+                    }
+                }
+            }
+            
+            // Checks South Corners
+            if((player == 2 || king) && j != 7){
+                // check SW
+                if(i != 0){
+                    if(board[i-1][j+1] == 0){
+                        // Adds move
+                        moves.push_back({Coordinate(i, j), Coordinate(i-1, j+1)});
+                    } else if((board[i-1][j+1] == otherPlayer ||board[i-1][j+1] == otherPlayer + 2)
+                              && i != 1 && j != 6){
+                        if(board[i-2][j+2] == 0){
+                            //***Jump
+                        }
+                    }
+                }
+                
+                // check SE
+                if(i != 7){
+                    if(board[i+1][j+1] == 0){
+                        // Adds move
+                        moves.push_back({Coordinate(i, j), Coordinate(i+1, j+1)});
+                    } else if((board[i+1][j+1] == otherPlayer ||board[i+1][j+1] == otherPlayer + 2)
+                              && i != 6 && j != 6){
+                        if(board[i+2][j+2] == 0){
+                            //***Jump
+                        }
+                    }
+                }
+            }
+        }
+        
+        if(i == 7){
+            i = -1;
+            j++;
+        }
+    }
+}
+
 void printBoard(){
     for(int i = 0, j = 0; j < 8; i++){
         cout << " | " << board[i][j];
@@ -50,6 +149,7 @@ void printBoard(){
 
 int main(int argc, const char * argv[]) {
     initStartBoard();
-    printBoard();
+    legalMoves(1);
+    
     return 0;
 }
